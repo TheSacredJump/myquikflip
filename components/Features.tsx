1da67fb144dba8e41dc8cf017baf0a8fb936f2ec
TheSacredJump <padhu.sathya@gmail.com>
2024-10-08 16:29:07 -0500

    Features fixes

--- components/Features.tsx
+++ components/Features.tsx
@@ -41,8 +41,6 @@ const FeatureContent = ({ title, description }: { title: string, description: st
         <FeatureContent
           title="Lightning-Fast Transactions"
           description="Experience the power of QuikFlip's instant conversion. Turn your crypto into spendable cash with just a few taps, making everyday purchases a breeze."
-          splineScene="https://prod.spline.design/9RHTTHtntQrUmjdu/scene.splinecode" // Use your Spline scene URL here
-          imageUrl="/Transaction.png"
         />
       ),
     },
@@ -50,13 +48,11 @@ const FeatureContent = ({ title, description }: { title: string, description: st
     category: "Multiple Currencies",
     title: "Support for Major Cryptocurrencies",
     src: "https://prod.spline.design/5dChaHNC0IF2STGj/scene.splinecode",
-    imageUrl: "/Multiple.png",
+    imageUrl: "/multiple.png",
     content: (
       <FeatureContent
         title="Diverse Crypto Portfolio"
         description="QuikFlip supports a wide range of cryptocurrencies, including Bitcoin, Ethereum, and more. Manage and convert multiple digital assets in one place."
-        splineScene="https://prod.spline.design/5dChaHNC0IF2STGj/scene.splinecode"
-        imageUrl="/Multiple.png"
       />
     ),
   },
@@ -69,8 +65,6 @@ const FeatureContent = ({ title, description }: { title: string, description: st
       <FeatureContent
         title="Your Security is Our Priority"
         description="Rest easy knowing your assets are protected by state-of-the-art encryption and multi-factor authentication. QuikFlip employs the latest security measures to keep your investments safe."
-        splineScene="https://prod.spline.design/5rIxZWqIatwHAvak/scene.splinecode"
-        imageUrl="/Bank.png"
       />
     ),
   },
@@ -83,8 +77,6 @@ const FeatureContent = ({ title, description }: { title: string, description: st
       <FeatureContent
         title="More Value for Your Crypto"
         description="Enjoy some of the lowest fees in the industry. QuikFlip's transparent pricing ensures you get the most out of your conversions, with no hidden charges."
-        splineScene="https://prod.spline.design/2bmt3o7zZI47WyqA/scene.splinecode"
-        imageUrl="/Fees.png"
       />
     ),
   },
@@ -97,8 +89,6 @@ const FeatureContent = ({ title, description }: { title: string, description: st
       <FeatureContent
         title="Simplicity Meets Functionality"
         description="Whether you're a crypto novice or an experienced trader, QuikFlip's user-friendly interface makes converting and managing your assets a seamless experience."
-        splineScene="https://prod.spline.design/Nzy0mCIkimZIs4hA/scene.splinecode"
-        imageUrl="/Interface.png"
       />
     ),
   },
@@ -111,8 +101,6 @@ const FeatureContent = ({ title, description }: { title: string, description: st
       <FeatureContent
         title="We're Here When You Need Us"
         description="Our dedicated support team is available 24/7 to assist you with any questions or concerns. Experience peace of mind with QuikFlip's reliable customer service."
-        splineScene="https://prod.spline.design/SZkOc6pEa02HapmL/scene.splinecode"
-        imageUrl="/Clock.png"
       />
     ),
   },
